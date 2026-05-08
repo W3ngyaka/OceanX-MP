39740f2fec506fe41a5a6d591a611a9ae4016a55
JunHeng <junhengweng@gmail.com>
2026-05-08 14:14:52 +0800

    dual monitor

--- OceanX MP/Assets/Scripts/DualMonitor.cs
+++ OceanX MP/Assets/Scripts/DualMonitor.cs
@@ -5,7 +5,10 @@ public class DualMonitor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+        }
     }
 
     // Update is called once per frame
